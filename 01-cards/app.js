197cb690c93e9ec030a05ddd6377af1809eff092
ICX\Mikhail.Shpakouski <mihailshpakovskii@gmail.com>
2021-09-15 09:09:37 +0300

    some change

--- 01-cards/app.js
+++ 01-cards/app.js
@@ -1,14 +1,19 @@
-const slides = document.querySelectorAll(".slide");
+const slidesPlugin = (active) => {
+  const slides = document.querySelectorAll(".slide");
 
-for (const slide of slides) {
-  slide.addEventListener("click", () => {
-      clearActiveClasses()
-      slide.classList.add('active')
-  });
-}
+  slides[active].classList.add("active");
 
-const clearActiveClasses = () => {
+  for (const slide of slides) {
+    slide.addEventListener("click", () => {
+      clearActiveClasses();
+      slide.classList.add("active");
+    });
+  }
+
+  const clearActiveClasses = () => {
     slides.forEach((slide) => {
-        slide.classList.remove('active')
-    })
-}
+      slide.classList.remove("active");
+    });
+  };
+};
+slidesPlugin(2);
